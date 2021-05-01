a27e95684ce38ed9145da6f72322c7ea9d67231f
Will Kelton <will.kelton@pm.me>
2021-05-01 17:51:55 -0500

    changes backup filenames to have timestamps instead of version numbers

--- lib/abackup/docker.py
+++ lib/abackup/docker.py
@@ -2,9 +2,12 @@ import logging
 import os
 import shlex
 import subprocess
+import time
 import uuid
 
 from typing import List
+
+from abackup import fs
 
 
 # TODO: look into replacing all the subprocess calls with the python docker library
@@ -96,14 +99,26 @@ class MySqlBRCommand(DockerCommand):
                  output_path: str = None):
         self.mysql_command = mysql_command
         self.name = name
+        self.file_prefix = MySqlBRCommand.default_file_prefix(name)
+        self.file_extension = MySqlBRCommand.default_file_extension()
         self.backup_file = MySqlBRCommand.default_backup_file(backup_path, name)
         d_opts = docker_options + ['-i']
         super().__init__('{} {} -p"{}" {}'.format(mysql_command, " ".join(mysql_options), password, name),
                          container_name, d_opts, input_path=input_path, output_path=output_path, in_container=True)
 
     @classmethod
+    def default_file_prefix(cls, name):
+        return name
+
+    @classmethod
+    def default_file_extension(cls):
+        return 'sql'
+
+    @classmethod
     def default_backup_file(cls, backup_path, name):
-        return os.path.join(backup_path, "{}.sql".format(name))
+        return os.path.join(backup_path, "{}_{}.{}".format(MySqlBRCommand.default_file_prefix(name),
+                                                           time.strftime("%Y%m%d_%H%M%S"),
+                                                           MySqlBRCommand.default_file_extension()))
 
     def friendly_str(self):
         return "{} {}".format(self.mysql_command, self.name)
@@ -116,28 +131,43 @@ class MysqlBackupCommand(MySqlBRCommand):
 
 
 class MysqlRestoreCommand(MySqlBRCommand):
-    def __init__(self, name: str, password: str, backup_path: str, container_name: str, docker_options: List[str]):
+    def __init__(self, name: str, password: str, backup_path: str, container_name: str, docker_options: List[str],
+                 backup_filename: str = None):
+        backup_filename = fs.find_youngest_file(backup_path, MySqlBRCommand.default_file_prefix(name),
+                                                MySqlBRCommand.default_file_extension()) if not backup_filename else backup_filename
         super().__init__(name, password, 'mysql', [], backup_path, container_name, docker_options,
-                         input_path=MySqlBRCommand.default_backup_file(backup_path, name))
+                         input_path=os.path.join(backup_path, backup_filename))
+        self.backup_file = os.path.join(backup_path, backup_filename)
 
 
 class PostgresBRCommand(DockerCommand):
     def __init__(self, name: str, postgres_command: str, postgres_options: List[str], backup_path: str,
-                 container_name: str,
-                 docker_options: List[str], user: str = None, password: str = None, input_path: str = None,
-                 output_path: str = None):
+                 container_name: str, docker_options: List[str], user: str = None, password: str = None,
+                 input_path: str = None, output_path: str = None):
         self.postgres_command = postgres_command
         self.name = name
         # TODO: support using 'password'
         p_opts = postgres_options + ['-U', user] if user else postgres_options
+        self.file_prefix = PostgresBRCommand.default_file_prefix(name)
+        self.file_extension = PostgresBRCommand.default_file_extension()
         self.backup_file = PostgresBRCommand.default_backup_file(backup_path, name)
         d_opts = docker_options + ['-i']
         super().__init__('{} {}'.format(postgres_command, " ".join(p_opts)), container_name, d_opts,
                          input_path=input_path, output_path=output_path, in_container=True)
 
     @classmethod
+    def default_file_prefix(cls, name):
+        return name
+
+    @classmethod
+    def default_file_extension(cls):
+        return 'sql'
+
+    @classmethod
     def default_backup_file(cls, backup_path, name):
-        return os.path.join(backup_path, "{}.sql".format(name))
+        return os.path.join(backup_path, "{}_{}.{}".format(PostgresBRCommand.default_file_prefix(name),
+                                                           time.strftime("%Y%m%d_%H%M%S"),
+                                                           PostgresBRCommand.default_file_extension()))
 
     def friendly_str(self):
         return "{} {}".format(self.postgres_command, self.name)
@@ -158,17 +188,21 @@ class PostgresBackupCommand(PostgresBRCommand):
 
 class PostgresRestoreCommand(PostgresBRCommand):
     def __init__(self, name: str, backup_path: str, container_name: str, docker_options: List[str], user: str,
-                 password: str,
-                 restore_all: bool = False):
+                 password: str, restore_all: bool = False, backup_filename: str = None):
+        backup_filename = fs.find_youngest_file(backup_path, PostgresBRCommand.default_file_prefix(name),
+                                                PostgresBRCommand.default_file_extension()) if not backup_filename else backup_filename
         super().__init__(name, 'psql', ['-d', 'postgres'] if restore_all else ['-d', name], backup_path,
                          container_name, docker_options, user, password,
-                         input_path=PostgresBRCommand.default_backup_file(backup_path, name))
+                         input_path=os.path.join(backup_path, backup_filename))
+        self.backup_file = os.path.join(backup_path, backup_filename)
 
 
 class DirectoryBackupCommand(DockerCommand):
     def __init__(self, directory: str, backup_path: str, container_name: str, docker_options: List[str]):
         self.directory = directory
-        tar_name = "{}.tar.gz".format(os.path.basename(directory))
+        self.file_prefix = os.path.basename(directory)
+        self.file_extension = 'tar.gz'
+        tar_name = "{}_{}.{}".format(self.file_prefix, time.strftime("%Y%m%d_%H%M%S"), self.file_extension)
         local_dir = "/abackup"
         self.local_file = os.path.join(local_dir, tar_name)
         self.tmp_dir = os.path.join(os.getcwd(), '.abackup-tmp', str(uuid.uuid4()))
@@ -190,9 +224,12 @@ class DirectoryBackupCommand(DockerCommand):
 
 
 class DirectoryRestoreCommand(DockerCommand):
-    def __init__(self, directory: str, backup_path: str, container_name: str, docker_options: List[str]):
+    def __init__(self, directory: str, backup_path: str, container_name: str, docker_options: List[str],
+                 tar_name: str = None):
         self.directory = directory
-        tar_name = "{}.tar.gz".format(os.path.basename(directory))
+        self.file_prefix = os.path.basename(directory)
+        self.file_extension = 'tar.gz'
+        tar_name = tar_name if tar_name else fs.find_youngest_file(backup_path, self.file_prefix, self.file_extension)
         local_dir = "/abackup"
         self.local_file = os.path.join(local_dir, tar_name)
         self.tmp_dir = os.path.join(os.getcwd(), '.abackup-tmp', str(uuid.uuid4()))
--- test/abackup/setup/init_postgres.sh
+++ test/abackup/setup/init_postgres.sh
@@ -4,4 +4,4 @@ docker-compose up --no-start test_postgres
 docker cp test_data/init_postgres.sql test-postgres:/docker-entrypoint-initdb.d
 docker-compose up -d test_postgres
 
-sleep 3
+sleep 15
